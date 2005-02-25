3a9699a5a4f7353db61b74bcc349e4958b81441e
andras <andras>
2005-02-25 01:32:34 +0000

    *** empty log message ***

--- makemake.cmd
+++ makemake.cmd
@@ -39,8 +39,8 @@ cd %root%\Util && %MAKEMAKE% %OPTS% -n -r -I. -I..\Mobility -I..\World -I..\Base
 
 :#---------------
 :#FIXME try to eliminate dep in NetworkInterfaces, Ethernet6, PPP6, Wireless, Mobility!!
-cd %root%\Network\IPv6 && %MAKEMAKE% %OPTS% -n -r -I..\..\NetworkInterfaces -I..\..\NetworkInterfaces\PPP6 -I..\..\NetworkInterfaces\Ethernet6 -I..\..\NetworkInterfaces\Wireless  -I..\HMIPv6  -I..\MIPv6 -I..\..\Util -I..\..\Util\Loki -I..\..\World -I..\..\Mobility -I. -I..\.. -I..\IPv4 -I..\..\Base
-cd %root%\Network\ICMPv6 && %MAKEMAKE% %OPTS% -n -r -I..\..\NetworkInterfaces -I..\..\NetworkInterfaces\PPP6 -I..\..\NetworkInterfaces\Ethernet6 -I..\..\NetworkInterfaces\Wireless  -I..\HMIPv6  -I..\MIPv6 -I..\..\Util -I..\..\Util\Loki -I..\..\World -I..\..\Mobility -I. -I..\.. -I..\IPv4 -I..\IPv6 -I..\ICMPv6 -I..\..\Base
+cd %root%\Network\IPv6 && %MAKEMAKE% %OPTS% -n -r -I..\ICMPv6 -I..\..\NetworkInterfaces -I..\..\NetworkInterfaces\PPP6 -I..\..\NetworkInterfaces\Ethernet6 -I..\..\NetworkInterfaces\Wireless  -I..\HMIPv6  -I..\MIPv6 -I..\..\Util -I..\..\Util\Loki -I..\..\World -I..\..\Mobility -I. -I..\.. -I..\IPv4 -I..\..\Base
+cd %root%\Network\ICMPv6 && %MAKEMAKE% %OPTS% -n -r -I..\IPv6 -I..\..\NetworkInterfaces -I..\..\NetworkInterfaces\PPP6 -I..\..\NetworkInterfaces\Ethernet6 -I..\..\NetworkInterfaces\Wireless  -I..\HMIPv6  -I..\MIPv6 -I..\..\Util -I..\..\Util\Loki -I..\..\World -I..\..\Mobility -I. -I..\.. -I..\IPv4 -I..\IPv6 -I..\ICMPv6 -I..\..\Base
 :#FIXME try to eliminate MIP dependency on HMIP and on Wireless!
 cd %root%\Network\MIPv6 && %MAKEMAKE% %OPTS% -n -r -I..\IPv6 -I..\ICMPv6 -I..\HMIPv6 -I..\..\NetworkInterfaces -I..\..\NetworkInterfaces\Ethernet6 -I..\..\NetworkInterfaces\Wireless -I..\..\Util -I..\..\Util\Loki -I..\..\World -I..\..\PHY -I. -I..\.. -I..\IPv4 -I..\..\Base
 cd %root%\Network\HMIPv6 && %MAKEMAKE% %OPTS% -n -r -I..\IPv6 -I..\ICMPv6 -I..\MIPv6 -I..\..\Util -I..\..\Util\Loki -I..\..\World -I..\..\PHY -I. -I..\.. -I..\IPv4 -I..\..\Base

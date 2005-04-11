43f06af41a83e0538a1da0689a8331de8113bd84
andras <andras>
2005-04-11 07:33:23 +0000

    *** empty log message ***

--- Applications/UDPApp/UDPVideoStreamSvr.cc
+++ Applications/UDPApp/UDPVideoStreamSvr.cc
@@ -1,14 +1,12 @@
-// -*- C++ -*-
-// Copyright (C) 2004 Johnny Lai
 //
-// This file is part of IPv6Suite
+// Copyright (C) 2005 Andras Varga
 //
-// IPv6Suite is free software; you can redistribute it and/or
+// This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 //
-// IPv6Suite is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
@@ -19,151 +17,96 @@
 //
 
 
-//Headers for libcwd debug streams have to be first (remove if not used)
-#include "sys.h"
-#include "debug.h"
+//
+// based on the video streaming app of the similar name by Johnny Lai
+//
 
-#include <boost/cast.hpp>
-#include <sstream>
 
 #include "UDPVideoStreamSvr.h"
-#include "UDPMessages_m.h"
-#include "opp_akaroa.h"
-#include "opp_utils.h" //auto_downcast
+#include "UDPControlInfo_m.h"
+#include "stlwatch.h"
+
 
 Define_Module(UDPVideoStreamSvr);
 
-using std::endl;
+inline std::ostream& operator<<(std::ostream& out, const UDPVideoStreamSvr::VideoStreamData& d) {
+    out << "client=" << d.clientAddr << ":" << d.clientPort
+        << "  total=" << d.videoSize << "  left=" << d.bytesLeft;
+    return out;
+}
+
 
 void UDPVideoStreamSvr::initialize()
 {
-  server = true; // must be before we invoke base class (this is bad design, but udpapplication needs to be rewritten anyway)
-  UDPApplBase::initialize();
-  assert(server == true);
-  //Leave address for now but can be used for listening on certain interfaces
-  //only
+    waitInterval = &par("waitInterval");
+    packetLen = &par("packetLen");
+    videoSize = &par("videoSize");
 
-  minWaitInt = par("minWaitInt");
-  maxWaitInt = par("maxWaitInt");
-  minPacketLen = par("minPacketLen");
-  maxPacketLen = par("maxPacketLen");
-  videoSize = par("videoSize");
-  speed = par("speed");
-
-  assert(videoSize > 0);
-  assert(maxWaitInt > 0);
-  assert(maxPacketLen > 0);
-
-  Dout(dc::udp_video_svr|flush_cf, fullPath()<<" pars for app "<<className()<<
-       " minWaitInt="<<minWaitInt<<" maxWaitInt="<<maxWaitInt<<" minPacketLen="
-       <<minPacketLen<<" maxPacketLen="<<maxPacketLen<<" videoSize="<<videoSize
-       <<" speed="<<speed);
-
-  cs = new ExpiryEntryList<ClientStreamData,
-                  Loki::cTimerMessageCB<void, TYPELIST_1(ClientStreamData)> >
-    (new Loki::cTimerMessageCB<void, TYPELIST_1(ClientStreamData)>
-       (6789, this, this, &UDPVideoStreamSvr::transmitClientStream,
-        "VideoStreamSvrThreads")
-     );
-
+    WATCH_PTRVECTOR(streamVector);
 }
 
 void UDPVideoStreamSvr::finish()
 {
 }
 
-static const float RNGFactor = 1000.0;
-
-void UDPVideoStreamSvr::handleMessage(cMessage* theMsg)
+void UDPVideoStreamSvr::handleMessage(cMessage *msg)
 {
-  std::auto_ptr<cMessage> msg(theMsg);
-  if (!isReady())
-  {
-    UDPApplBase::handleMessage(msg.get());
-    return;
-  }
-
-  if (msg->isSelfMessage())
-  {
-    //Timer for a particular client so send packet to them
-    cTimerMessage* noDelete =
-      check_and_cast<cTimerMessage*>(msg.release());
-    noDelete->callFunc();
-    return;
-  }
-
-
-  ///processRequest
-
-   std::auto_ptr<UDPAppInterfacePacket> pkt =
-    OPP_Global::auto_downcast<UDPAppInterfacePacket>(msg);
-
-   std::string src = ipv6_addr_toString(pkt->getSrcIPAddr()); // FIXME why convert to string... Andras
-   UDPPacketBase* udpPkt =
-     check_and_cast<UDPPacketBase*>(pkt->encapsulatedMsg());
-   double pause = OPP_UNIFORM(minWaitInt*RNGFactor, maxWaitInt*RNGFactor)/RNGFactor;
-
-   ClientStreamData d(src, udpPkt->getSrcPort(), videoSize, pause + simTime());
-
-   if (cs->findEntry(d))
-     Dout(dc::udp_video_svr, fullPath()<<" "<<simTime()<<" "<<src<<":"<<d.port
-          <<" requested stream again so it is now reset");
-   else
-     Dout(dc::udp_video_svr, fullPath()<<" "<<simTime()<<" "<<src<<":"<<d.port
-           <<" requested stream");
-
-   ///Re/start the video streaming based on pause calculated now
-   cs->addEntry(d);
-
+    if (msg->isSelfMessage())
+    {
+        // timer for a particular video stream expired, send packet
+        transmitClientStream(msg);
+    }
+    else
+    {
+        // start streaming
+        processStreamRequest(msg);
+    }
 }
 
-/*
-  @arg c is rather misleading because we never actually assign the callback
-  argument. Instead we get it from cs->smallestExpiryEntry.
- */
-void UDPVideoStreamSvr::transmitClientStream(ClientStreamData& c)
+
+void UDPVideoStreamSvr::processStreamRequest(cMessage *msg)
 {
-  //Read in c
-  assert(cs->smallestExpiryEntry(c));
+    // register video stream...
+    UDPControlInfo *ctrl = check_and_cast<UDPControlInfo *>(msg->controlInfo());
 
-  UDPPacketBase* rudpPkt = new UDPPacketBase;
-  rudpPkt->setSrcPort(port);
-  rudpPkt->setDestPort(c.port);
-  //rudpPkt->setKind()  //Can be used for sequence numbers to detect missing pkts
-  unsigned int pkt_size = (unsigned int) OPP_UNIFORM(minPacketLen, maxPacketLen);
-  pkt_size = std::min(pkt_size, c.bytesLeft);
-  rudpPkt->setLength(pkt_size);
-  UDPAppInterfacePacket* rpkt = new UDPAppInterfacePacket;
-  rpkt->setKind(KIND_DATA);
-  rpkt->setDestIPAddr(c_ipv6_addr(c.host.c_str())); // FIXME yet another $#%# conversion....
-  rpkt->encapsulate(rudpPkt);
-  rudpPkt->setTimestamp(simTime());
+    VideoStreamData *d = new VideoStreamData;
+    d->clientAddr = ctrl->getSrcAddr();
+    d->clientPort = ctrl->getSrcPort();
+    d->videoSize = (*videoSize);
+    d->bytesLeft = d->videoSize;
+    streamVector.push_back(d);
 
-  c.bytesLeft -= pkt_size;
-  c.packetCount++;
+    cMessage *timer = new cMessage("VideoStreamTmr");
+    timer->setContextPointer(d);
 
-  Dout(dc::udp_video_svr|continued_cf, fullPath()<<" "<<simTime()<<" packet of size "
-       <<pkt_size<<" sent to "<<c.host<<":"<<c.port<<" bytes Left="<<c.bytesLeft);
+    // ... then transmit first packet right away
+    transmitClientStream(timer);
+}
 
-  if (c.bytesLeft != 0)
-  {
-    double pause = OPP_UNIFORM(minWaitInt*RNGFactor, maxWaitInt*RNGFactor)/RNGFactor;
-    c.txTime = simTime() + pause;
-    cs->addEntry(c);
-    Dout(dc::finish, " Stream scheduled for "<< c.txTime);
-  }
-  else
-  {
-    //end of stream indicator. This means seq no. start from 1.
-    rudpPkt->setKind(0);
 
-    cs->removeExpiredEntry(Loki::Int2Type<Loki::TypeTraits<CSEL::ElementType>::isPointer>());
-    Dout(dc::finish, " Stream finished with"<<c.packetCount<<" packets sent");
-    std::ostringstream os;
-    os<<c.host<<":"<<c.port<<" packet count";
-    recordScalar(os.str().c_str(), c.packetCount);
-  }
+void UDPVideoStreamSvr::transmitClientStream(cMessage *timer)
+{
+    VideoStreamData *d = (VideoStreamData *) timer->contextPointer();
 
-  send(rpkt, gateId);
+    // generate and send a packet
+    cMessage *pkt = new cMessage("VideoStrmPk");
+    int pktLen = (*packetLen);
+    if (pktLen > d->bytesLeft)
+        pktLen = d->bytesLeft;
+    pkt->setLength(8*pktLen);
+    sendToUDP(pkt, udpPort, d->clientAddr, d->clientPort);
 
+    d->bytesLeft =- pktLen;
+
+    // reschedule timer if there's bytes left to send
+    if (d->bytesLeft==0)
+    {
+        double interval = (*waitInterval);
+        scheduleAt(simTime()+interval, timer);
+    }
+    else
+    {
+        delete timer;
+        // TBD find VideoStreamData in streamVector and delete it
+    }
 }
--- Applications/UDPApp/UDPVideoStreamSvr.h
+++ Applications/UDPApp/UDPVideoStreamSvr.h
@@ -1,14 +1,12 @@
-// -*- C++ -*-
-// Copyright (C) 2004 Johnny Lai
 //
-// This file is part of IPv6Suite
+// Copyright (C) 2005 Andras Varga
 //
-// IPv6Suite is free software; you can redistribute it and/or
+// This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 //
-// IPv6Suite is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
@@ -19,95 +17,65 @@
 //
 
 
+//
+// based on the video streaming app of the similar name by Johnny Lai
+//
+
 #ifndef UDPVIDEOSTREAMSVR_H
 #define UDPVIDEOSTREAMSVR_H
 
 #include <vector>
-#include <map>
-#include <string>
 #include <omnetpp.h>
-#include "UDPApplBase.h"
-#include "ExpiryEntryList.h"
+#include "UDPAppBase.h"
 
 
 /**
  * Stream VBR video streams to clients.
  *
- * Implements the UDPApplBase interface and cooperates with UDPVideoStreamCli.
- * UDPVideoStreamCli requests a stream and UDPVideoStreamSvr starts streaming to
- * them. Capable of handling streaming to multiple clients.
- *
+ * Cooperates with UDPVideoStreamCli. UDPVideoStreamCli requests a stream
+ * and UDPVideoStreamSvr starts streaming to them. Capable of handling
+ * streaming to multiple clients.
  */
-class UDPVideoStreamSvr: public UDPApplBase
+class UDPVideoStreamSvr : public UDPAppBase
 {
-public:
+  public:
+    /**
+     * Stores information on a video stream
+     */
+    struct VideoStreamData
+    {
+        IPvXAddress clientAddr;  ///< client address
+        int clientPort;          ///< client UDP port
+        long videoSize;          ///< total size of video
+        long bytesLeft;          ///< bytes left to transmit
+    };
 
-  Module_Class_Members(UDPVideoStreamSvr, UDPApplBase, 0);
+  protected:
+    typedef std::vector<VideoStreamData *> VideoStreamVector;
+    VideoStreamVector streamVector;
 
-  ///@name Overidden cSimpleModule functions
-  //@{
-  virtual void initialize();
+    // module parameters
+    int udpPort;
+    cPar *waitInterval;
+    cPar *packetLen;
+    cPar *videoSize;
 
-  virtual void finish();
+  protected:
+    // process stream request from client
+    void processStreamRequest(cMessage *msg);
 
-  virtual void handleMessage(cMessage* msg);
-  //@}
+    // send a packet of the given video stream
+    void transmitClientStream(cMessage *timer);
 
-protected:
+  public:
+    Module_Class_Members(UDPVideoStreamSvr, UDPAppBase, 0);
 
-private:
-  double minWaitInt;
-  double maxWaitInt;
-  unsigned int minPacketLen;
-  unsigned int maxPacketLen;
-  unsigned int videoSize;
-  unsigned int speed;
-
-  /**
-     @struct ClientStreamData
-
-     @brief Stores information on the client that has requested video streaming
-
-     Compiler generated copy ctor is fine as long as no pointers are used
-   */
-  struct ClientStreamData
-  {
-    ///Satisfy default constructible req. by providing defaults
-    ClientStreamData(std::string host = std::string(""), unsigned int port = 0,
-                     unsigned int bytesLeft = 0, double txTime = 0)
-      :host(host), port(port), bytesLeft(bytesLeft), txTime(txTime),
-       packetCount(0)
-      {}
-
-    ///peer host ID (IPv6 address)
-    std::string host;
-    ///peer port
-    unsigned int port;
-    unsigned int bytesLeft;
-    ///Next scheduled stream transmission time
-    double txTime;
-    ///number of pkt tx (not constant as each packet size is random)
-    unsigned int packetCount;
-    double expiryTime() const { return txTime; }
-    bool operator==(const ClientStreamData& rhs) const
-      {
-        return this->port == rhs.port && this->host == rhs.host;
-      }
-  };
-
-  //  std::map<std::string, ClientStreamData> cs;
-
-  ///List of clients that have requested streams from me
-  typedef ExpiryEntryList<
-    ClientStreamData, Loki::cTimerMessageCB<void, TYPELIST_1(ClientStreamData)>
-  > CSEL;
-
-  CSEL* cs;
-
-  ///Callback invoked to send stream to client c when client is scheduled to
-  ///receive a stream as determined by cs.
-  void transmitClientStream(ClientStreamData& c);
-
+    ///@name Overidden cSimpleModule functions
+    //@{
+    virtual void initialize();
+    virtual void finish();
+    virtual void handleMessage(cMessage* msg);
+    //@}
 };
 
 

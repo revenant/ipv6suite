c9652d24bbf3456edb5e581f978555f329a129db
ewu <ewu>
2005-06-09 08:31:56 +0000

    *** empty log message ***

--- Research/Networks/MIPv6Network/run5.sh
+++ Research/Networks/MIPv6Network/run5.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-  echo "Please enter the number of runs required (eg. <no. of runs>)" 
+  echo "Please enter the name of scenario and the number of runs required (eg. <name> <no. of runs>)" 
   exit 1
 fi
 
-let "numberOfSeeds=${1}"
+let "numberOfSeeds=${2}"
 setRun=1
 
 seedtool g 1 10000000 $numberOfSeeds > seeds.txt
@@ -20,7 +20,7 @@ output-vector-file = MN-MNComms-${setRun}.vec
 seed-lcg32 = ${seed}
 EOF
      ./cmdSimulMove2 -f omnetpp${set}-${setRun}.ini
-     cat "MN-MNComms-${setRun}.vec" >> MN-MNComms.vec
+     cat "MN-MNComms-${setRun}.vec" >> MN-MNComms_${1}.vec
      seed=`expr $seed + 1`
 done
     

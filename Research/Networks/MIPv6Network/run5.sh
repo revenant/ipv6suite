1270c3350cfb3dc942b0d9bcaf64cc22d2f4dde6
ewu <ewu>
2005-06-13 11:37:52 +0000

    *** empty log message ***

--- Research/Networks/MIPv6Network/run5.sh
+++ Research/Networks/MIPv6Network/run5.sh
@@ -8,19 +8,21 @@ fi
 
 let "numberOfSeeds=${2}"
 setRun=1
-
+             
+cp SimulMove2.ini SimulMove2_${1}.ini
 seedtool g 1 10000000 $numberOfSeeds > seeds.txt
-
 for seed in `cat seeds.txt`;
 do
-     cp omnetpp${set}.ini omnetpp${set}-${setRun}.ini
-     cat <<EOF >>omnetpp${set}-${setRun}.ini
-[General]
+     cat <<EOF >>SimulMove2_${1}.ini
+[Run ${setRun}]
+network = mipv6NetworkSimulMove2
 output-vector-file = MN-MNComms-${setRun}.vec
-seed-lcg32 = ${seed}
+seed_lcg32 = ${seed}
+include ../../../Etc/default.ini
 EOF
-     ./cmdSimulMove2 -f omnetpp${set}-${setRun}.ini
+     ../../../bin/INET -u Cmdenv -f SimulMove2_${1}.ini -r ${setRun}
      cat "MN-MNComms-${setRun}.vec" >> MN-MNComms_${1}.vec
+#     rm omnetpp-${setRun}.ini
      setRun=`expr $setRun + 1`
 done
     

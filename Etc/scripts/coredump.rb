425ad57b1f89cd234ec34731ac572c1c3c9af71e
Johnny Lai <johnny.lai@eng.monash.edu.au>
2006-10-05 22:26:19 +1000

    check core dump script
    does not reduce to unique set yet

---
+++ Etc/scripts/coredump.rb
@@ -0,0 +1,30 @@
+File.open("debug.txt", "w"){|f|
+f.puts <<END
+bt
+q
+END
+}
+hash = {}
+dumps = []
+Dir["core.*"].each{|corefile|
+  dump = `gdb ./Comparison -q -x debug.txt #{corefile}|grep ^\#`
+  if dump =~ /\?\?/m
+  puts "#{corefile} is stuffed i.e. no bt so deleting"
+  File.delete(corefile)
+else
+  #puts dump.gsub(/^[^#].*$/m,"")
+  #dump = `echo #{dump}|grep ^#` 
+#dump.split('\n').each{|l|
+ #puts l if l =~ /^#.*$/
+#}
+
+if false
+puts "corefile #{corefile} dumped:"
+puts dump
+end
+dumps << dump if not dumps.include?(dump)
+hash[corefile] = dumps.index(dump)
+end
+}
+puts "There were #{dumps.size} unique dumps out of #{hash.size}"
+puts dumps.to_s

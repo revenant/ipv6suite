revenant/ipv6suite

commit efe63839476f7aabd4c3b2d2eb32209882e67dd9

cTimerMessage::dtor remove self from FES. More tests added.
messages in FES after timer message deleted may have caused subtle problems
before. Method of removing them does not cause core dump anymore in omnet.

Network/MIPv6/MIPv6NDStateHost.cc
@@ -466,10 +466,9 @@ std::auto_ptr<RA> MIPv6NDStateHost::processRtrAd(std::auto_ptr<RA> rtrAdv)
   if (!mipv6cdsMN->primaryHA() && !awayFromHome() && isHA)
   {
     mipv6cdsMN->primaryHA() = bha;
-    mipv6cdsMN->_homeAddr = mipv6cdsMN->primaryHA()->prefix();
-    mipv6cdsMN->_homeAddr.prefix =
-      mipv6cdsMN->formCareOfAddress(mipv6cdsMN->primaryHA(),
-                                    ift->interfaceByPortNo(ifIndex));
+    bool primaryHoa = true;
+    mipv6cdsMN->formHomeAddress(mipv6cdsMN->primaryHA(), 
+				ift->interfaceByPortNo(ifIndex), primaryHoa);
 
     Dout(dc::mipv6|dc::mobile_move, rt->nodeName()<<" Primary HA registered. Addr="<<bha->addr());
   }
@@ -1081,10 +1080,8 @@ void MIPv6NDStateHost::relinquishRouter(boost::shared_ptr<MIPv6RouterEntry> oldR
   for (MIPv6RouterEntry::OPLI it = oldRtr->prefixes.begin(); it != oldRtr->prefixes.end(); it++)
   {
 
-    //wrong on-link does not refer to scope of address. We must remove all
-    //on-link address at old router except home address and rcoas formed from
-    //valid maps
-    if (!home_addr_scope_check((*it).prefix))
+    //assuming that rcoas not formed from RA's prefixes so are not removed here
+    if (mipv6cdsMN->homeAddr() != (*it).prefix)
     {
       IPv6Address addr = oie->ipv6()->matchPrefix((*it).prefix, (*it).length);
       if (addr != IPv6_ADDR_UNSPECIFIED)
@@ -1092,7 +1089,6 @@ void MIPv6NDStateHost::relinquishRouter(boost::shared_ptr<MIPv6RouterEntry> oldR
         Dout(dc::mipv6, rt->nodeName()<<__FUNCTION__
              <<":  onlink local addresss "<<addr<<" removed");
         rt->removeAddress(addr, oldIfIndex);
-	//TODO check if this effectively removes callback when message deleted
 	delete addressCallback(addr);
       }
       else if (oie->ipv6()->matchPrefix((*it).prefix, (*it).length), true)

Network/ICMPv6/NDStateRouter.cc
@@ -511,39 +511,11 @@ void  NDStateRouter::sendRtrAd(RS* rtrSol)
         nd->scheduleAt(nd->simTime() + delay, msg);
         break;
       }
-/*   //Previous version
-      if (msg->arrivalTime() < msg->sendingTime() + delay)
-        //The next scheduled time is fine so just wait till then to send a rtrAdv
-        break;
-        //last adv was sent within MIN_DELAY_BETWEEN_RAS so rate limiting to one
-        //adv for every MIN_DELAY_BETWEEN_RAS seconds
-      else if (nd->simTime() < msg->sendingTime() + MIN_DELAY_BETWEEN_RAS )
-      {
-        delay = delay + MIN_DELAY_BETWEEN_RAS;
-         nd->cancelEvent(msg);
-         nd->scheduleAt(nd->simTime() + delay, msg);
-         break;
-      }
-      //No need to rate limit just send it slightly earlier than the
-      //previous scheduled unsolicited adv
-      else
-      {
-        nd->cancelEvent(msg);
-        nd->scheduleAt(nd->simTime() + delay, msg);
-        break;
-      }
-*/
     }
 
   //Update neighbour cache if cond is met
   if (found && dgram->srcAddress() != IPv6_ADDR_UNSPECIFIED)
   {
-#if defined TESTIPv6 || defined DEBUG_RTRADV
-    cout <<rt->nodeName()<<":"<<tmr->ifIndex<<" "<<nd->simTime()
-         <<" SolRtrAdv sendingTime: "<<setprecision(4)<<msg->sendingTime()
-         <<" arivalTime: "<<setprecision(6)<<msg->arrivalTime()
-         <<" nextAdvDelay:"<<setprecision(6)<<delay<<"\n";
-#endif //#if defined TESTIPv6 || defined DEBUG_RTRADV
     Dout(dc::debug, rt->nodeName()<<":"<<tmr->ifIndex<<" "<<nd->simTime()
          <<" SolRtrAdv sendingTime: "<<setprecision(4)<<msg->sendingTime()
          <<" arivalTime: "<<setprecision(6)<<msg->arrivalTime()

Research/Networks/TestNetwork/CMakeLists.txt
@@ -12,4 +12,5 @@ COLLECT_CHECK_TEST(PingStreamODAD)
 ADD_TEST(PingStream TestNetwork -r 9)
 COLLECT_CHECK_TEST(PingStream)
 ADD_TEST(RtrAdvInt TestNetwork -r 10)
+ADD_TEST(RtrAdvIntMIPv6 TestNetwork -r 11)
 ADD_TEST(ValidateSignalling ${RUBY_EXECUTABLE} test_TestNetwork.rb)

Research/Networks/TestNetwork/omnetpp.ini
@@ -28,7 +28,7 @@ seed-16-mt = 1453732919
 seed-17-mt = 1453732920
 
 [Cmdenv]
-;module-messages = no
+module-messages = no
 event-banners=no
 
 [Tkenv]
@@ -144,6 +144,16 @@ sim-time-limit=100
 **.rtr.**.routingTable6.routePackets = true
 **.NWIName="IPv6PPPInterface"
 output-vector-file = rtr.vec
+
+[Run 11]
+network = testRouter
+sim-time-limit=20
+
+**.IPv6routingFile = xmldoc("TestNetwork3.xml")
+**.rtr.**.routingTable6.routePackets = true
+**.rtr.**.routingTable6.mobileIPv6Support = true
+**.NWIName="IPv6PPPInterface"
+output-vector-file = rtrmip6.vec
 
 [Parameters]
 **.networkLayer.proc.forwarding.routingInfoDisplay = true

Research/Networks/TestNetwork/test_TestNetwork.rb
@@ -100,17 +100,38 @@ def test_RA
   require 'rsruby'
   r = RSRuby.instance
   wait = `ruby ~/src/IPv6SuiteWithINET/Etc/scripts/RImportOmnet.rb rtr.vec`
-  if $?.exitstatus > 0
-    raise wait
-  end
+  raise wait if $?.exitstatus > 0
   r.load("test2.Rdata")
   data=r.a_RA_recv_client1_1
   diffs = r.diff(data["time"])
   doc = Document.new File.new("TestNetwork3.xml")
   rtr = doc.elements["*/local[@node='rtr']/interface"]
   assert(diffs.max <= rtr.attributes["MaxRtrAdvInterval"].to_f, "maximum #{diffs.max} should be less than in XML #{rtr.attributes["MaxRtrAdvInterval"].to_f}")  
-  assert(diffs.min >= rtr.attributes["MinRtrAdvInterval"].to_f, "minimum should be greater than in XML")  
   
+  assert(diffs.min > 3, "RA response to RS should always be at least \
+MIN_DELAY_BETWEEN_RAS (3 in spec)")
+  raise "min is not in array?" if diffs.delete(diffs.min).nil?
+
+  #unsolicited RAs have to satisfy this
+  assert(diffs.min >= rtr.attributes["MinRtrAdvInterval"].to_f, 
+         "Unsolicited RAs separation in sim #{diffs.min} should always be \ 
+greater than in XML #{rtr.attributes["MinRtrAdvInterval"].to_f}")  
+
+
+  wait = `rm test2.Rdata`
+  wait = `ruby ~/src/IPv6SuiteWithINET/Etc/scripts/RImportOmnet.rb rtrmip6.vec`
+  raise wait if $?.exitstatus > 0
+  r.rm(:list => r.ls.call)
+  r.load("test2.Rdata")
+  #Need to use r.get as r.a_RA_recv_client1_1 returns cached !!!
+  data = r.get("a.RA.recv.client1.1")
+  diffs = r.diff(data["time"])
+  assert(diffs.max <= rtr.attributes["MIPv6MaxRtrAdvInterval"].to_f, "maximum #{diffs.max} should be less than in XML #{rtr.attributes["MIPv6MaxRtrAdvInterval"].to_f}")  
+
+  assert(diffs.min >= rtr.attributes["MIPv6MinRtrAdvInterval"].to_f, 
+         "Unsolicited RAs separation in sim #{diffs.min} should always be \ 
+greater than in XML #{rtr.attributes["MIPv6MinRtrAdvInterval"].to_f}")  
+
 end
 
 def setup

Util/cTimerMessage.h
@@ -69,8 +69,11 @@ class cTimerMessage: public cMessage, boost::noncopyable
       //deleted.
       if (isScheduled())
       {
-	//fprintf(stderr, "%s in %s at %f is cancelled in dtor \n", name(), mod->fullPath().c_str(), mod->simTime());
-	//cancel();
+	fprintf(stderr, "%s in %s at %f is cancelled in dtor \n", name(), mod->fullPath().c_str(), mod->simTime());
+	cModule* context = simulation.contextModule();
+	simulation.setContextModule(mod);
+	cancel();
+	simulation.setContextModule(context);
       }
     }
 

commit 206f535fbf87fd9437dae748c068309431f366e5

fixed regression introduced in efe63839476f7aabd4c3b2d2eb32209882e67dd9
where supposedly home address was preserved.

Introduced callbacks for MIPv6 sendBU instead of relying on dupAddrDetSuccess
to always send a bu which is not always correct as is case of odad on and get
repeated sending of bu. Not sure why this was not caught before. This fixes
a setFutureCoa assertion.

Required to set Neighbour Entry of revisted routers to reachable otherwise BUs
unable to reach "wire".

send NA after BU acc. to 11.5.4 of RFC 3775

Compared against 4ebd3b9897f6f8f8d8838a05aabb6647619fd9f0 where setFutureCoa
assertion did not happen (wcmc_y_22 -r1). downstream loss is almost same as
upstream now.

Network/ICMPv6/NDStateHost.cc
@@ -160,6 +160,8 @@ NDStateHost::~NDStateHost()
       nd->cancelEvent(*it);
       delete *it;
     }
+
+  removeAllCallbacks();
 }
 
 std::auto_ptr<ICMPv6Message> NDStateHost::processMessage(std::auto_ptr<ICMPv6Message> msg)
@@ -519,20 +521,11 @@ void NDStateHost::dupAddrDetSuccess(NDTimer* tmr)
     ifStats[tmr->ifIndex].initStarted = true;
   }
 
-  InterfaceEntry *ie = ift->interfaceByPortNo(tmr->ifIndex);
   nd->nb->fireChangeNotification(NF_IPv6_ADDR_ASSIGNED);
 
 #if USE_MOBILITY
-  if (rt->mobilitySupport() && rt->isMobileNode() && rt->mipv6cds->mipv6cdsMN->awayFromHome())
-  {
-    ipv6_addr potentialCoa = ie->ipv6()->inetAddrs[ie->ipv6()->inetAddrs.size()-1];
-    MobileIPv6::MIPv6NDStateHost* mipv6StateMN =
-      boost::polymorphic_downcast<MobileIPv6::MIPv6NDStateHost*> (this);
-    assert(mipv6StateMN);
-    Dout(dc::debug, rt->nodeName()<<" potential coa in dupAddrDetSuc "<<potentialCoa);
-    mipv6StateMN->sendBU(potentialCoa);
-    invokeCallback(potentialCoa);
-  }
+  if (rt->mobilitySupport() && rt->isMobileNode())
+    invokeCallback(tmr->tentativeAddr);
 #endif //USE_MOBILITY
 
 }
@@ -733,6 +726,13 @@ std::auto_ptr<RA> NDStateHost::processRtrAd(std::auto_ptr<RA> rtrAdv)
           //Set state to STALE when LL addr changed
           re->setState(NeighbourEntry::STALE);
         }
+	else if (rt->mobilitySupport() && rtrAdv->hasSrcLLAddr() && rtrAdv->srcLLAddr() == re->linkLayerAddr() &&
+		 re->state() == NeighbourEntry::INCOMPLETE)
+	{
+	  //as MIPv6NDStateHost::relinquishRouter sets it to incomplete and now
+	  //mn has moved back to prev visited subnet.
+	  re->setState(NeighbourEntry::REACHABLE);
+	}
 
         reValid = true;
 #ifdef USE_MOBILITY
@@ -1378,12 +1378,25 @@ void NDStateHost::invokeCallback(const ipv6_addr& tentativeAddr)
   }
 }
 
-///@todo TODO convert addressCallbacks to boost::function
 cTimerMessage*  NDStateHost::addressCallback(const ipv6_addr& tentativeAddr)
 {
   if (!addressCallbacks.count(tentativeAddr))
     return 0;
   return addressCallbacks[tentativeAddr];
+}
+
+int NDStateHost::removeAllCallbacks()
+{
+  int ret = addressCallbacks.size();
+  typedef std::map<ipv6_addr, cTimerMessage*>::iterator AIT;
+  for (AIT it = addressCallbacks.begin(); it != addressCallbacks.end(); it++)
+  {
+    if (it->second->isScheduled())
+      it->second->cancel();
+    delete it->second;
+  }
+  addressCallbacks.clear();
+  return ret;
 }
 
 } //namespace IPv6NeighbourDiscovery

Network/ICMPv6/NDStateHost.h
@@ -174,6 +174,8 @@ namespace IPv6NeighbourDiscovery
     void addCallbackToAddress(const ipv6_addr& tentativeAddr, cTimerMessage*);
     bool callbackAdded(const ipv6_addr& tentativeAddr, int message_id);
     cTimerMessage* addressCallback(const ipv6_addr& tentativeAddr);
+    //returns no. of callbacks removed
+    int removeAllCallbacks();
 
   protected:
     std::vector<InterfaceStatus> ifStats;

Network/MIPv6/MIPv6NDStateHost.cc
@@ -524,8 +524,19 @@ std::auto_ptr<RA> MIPv6NDStateHost::processRtrAd(std::auto_ptr<RA> rtrAdv)
          mipv6cdsMN->movementDetected() ||
          mipv6cdsMN->eagerHandover()))
     {
-      Dout(dc::debug|flush_cf, rt->nodeName()
-           <<"Archaic movement detection triggered handover?");
+      if (rt->cds->neighbour(mipv6cdsMN->currentRouter()->re.lock()->addr()).lock().get() == 0)
+	Dout(dc::debug|flush_cf, rt->nodeName()
+           <<"Archaic movement detection triggered by currentRouter not reachable in AR");
+      else if (mipv6cdsMN->movementDetected())
+	Dout(dc::debug|flush_cf, rt->nodeName()
+           <<"Archaic movement detection triggered by movement Detected");
+      else if (mipv6cdsMN->eagerHandover())
+	Dout(dc::debug|flush_cf, rt->nodeName()
+           <<"Archaic movement detection triggered by eagerHandover");
+      else
+	Dout(dc::debug|flush_cf, rt->nodeName()
+	     <<"Archaic movement detection prob from NUD triggered handover?");
+
 #ifdef USE_HMIP
       if (rt->hmipSupport() && rtrAdv->hasMapOptions())
         Dout(dc::hmip, rt->nodeName()<<" Detected map options in MIPv6NDStateHost::processRtrAdv deferring handover(archaic branch) ");
@@ -996,6 +1007,9 @@ void MIPv6NDStateHost::handover(boost::shared_ptr<MIPv6RouterEntry> newRtr)
         Dout(dc::mipv6|dc::neighbour_disc|flush_cf, rt->nodeName()<<" "<<nd->simTime()
              <<ifIndex<<" waiting for dad completion before sending BU for coa "
              <<coa);
+	cSignalMessage* cb = new cSignalMessage("MIPv6sendBU");
+	cb->connect(boost::bind(&MIPv6NDStateHost::sendBU, this, coa));
+	addCallbackToAddress(coa, cb);
       }
 
     } //if primary HA exists
@@ -1058,6 +1072,8 @@ void MIPv6NDStateHost::relinquishRouter(boost::shared_ptr<MIPv6RouterEntry> oldR
        <<" missedAdvTimer reset as moved to new subnet potentialNewInterval="
        <<potentialNewInterval);
 
+  removeAllCallbacks();
+
   if (!oldRtr)
   {
     if (newRtr && mipv6cdsMN->previousDefaultRouter() && newRtr != mipv6cdsMN->previousDefaultRouter())
@@ -1081,24 +1097,23 @@ void MIPv6NDStateHost::relinquishRouter(boost::shared_ptr<MIPv6RouterEntry> oldR
   {
 
     //assuming that rcoas not formed from RA's prefixes so are not removed here
-    if (mipv6cdsMN->homeAddr() != (*it).prefix)
-    {
       IPv6Address addr = oie->ipv6()->matchPrefix((*it).prefix, (*it).length);
-      if (addr != IPv6_ADDR_UNSPECIFIED)
+      if (addr != IPv6_ADDR_UNSPECIFIED && addr != mipv6cdsMN->homeAddr())
       {
         Dout(dc::mipv6, rt->nodeName()<<__FUNCTION__
              <<":  onlink local addresss "<<addr<<" removed");
         rt->removeAddress(addr, oldIfIndex);
 	delete addressCallback(addr);
       }
-      else if (oie->ipv6()->matchPrefix((*it).prefix, (*it).length), true)
+      else
       {
-        //Must be tentative addrs
-        DoutFatal(dc::core|error_cf, "Unimplemented func for removing tentative addr when node moves "
-                  <<" too fast as dad timer has to be removed cleanly see "
-                  <<" checkDupAddrDetected and separate the bits there into separate func");
+	addr = oie->ipv6()->matchPrefix((*it).prefix, (*it).length, true);
+	if (addr != IPv6_ADDR_UNSPECIFIED && addr != mipv6cdsMN->homeAddr())
+	  //Must be tentative addrs
+	  DoutFatal(dc::core|error_cf, "Unimplemented func for removing tentative addr when node moves "
+		    <<" too fast as dad timer has to be removed cleanly see "
+		    <<" checkDupAddrDetected and separate the bits there into separate func");
       }
-    }
 
     Dout(dc::prefix_timer|flush_cf, rt->nodeName()<<" removing on link prefix="
          <<*it<<" due to movement");
@@ -1326,6 +1341,13 @@ void MIPv6NDStateHost::checkDecapsulation(IPv6Datagram* dgram)
     }
 }
 
+void sendNALater(cTimerMessage* tmr)
+{
+  tmr->callFunc();
+  delete static_cast<cCallbackMessage*>(tmr->contextPointer());
+  delete tmr;
+}
+
 /**
  * Does the actual procedure of returning home.
  *
@@ -1352,14 +1374,18 @@ void MIPv6NDStateHost::returnHome()
   }
 
   //send BU to HA A and H bit set coa = hoa and no home address option
-
   mstateMN->sendBUToAll(mipv6cdsMN->homeAddr(), mipv6cdsMN->homeAddr(), 0);
 
   //unsolicited NA to advertise our Link layer address for each on-link
   //prefix (just home addr for now) after BU sent.
-
-  //delay by 2*SELF_SCHEDULE_DELAY to ensure after BU sent (1*SELF_SCHEDULE_DELAY)
-  sendUnsolNgbrAd(mipv6cdsMN->primaryHA()->re.lock()->ifIndex(), mipv6cdsMN->homeAddr());
+  cCallbackMessage* cbuna = new cCallbackMessage;
+  (*cbuna) = boost::bind(&MIPv6NDStateHost::sendUnsolNgbrAd, this, mipv6cdsMN->primaryHA()->re.lock()->ifIndex(),
+	      mipv6cdsMN->homeAddr());
+  cCallbackMessage* callLater = new cCallbackMessage;
+  (*callLater) = boost::bind(sendNALater, cbuna);
+  cbuna->setContextPointer(callLater);
+  //ensure NA after BU sent
+  nd->scheduleAt(nd->simTime() + 100.0 * SELF_SCHEDULE_DELAY, callLater);
 
 #ifdef USE_HMIP
   if (rt->hmipSupport())
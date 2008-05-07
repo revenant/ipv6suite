revenant/ipv6suite

commit 9206b5ea68e77e5990141a4c2ee7fe222f45ba45

fix assertion at arhandover due to unassigned lcoa

Network/HMIPv6/HMIPv6NDStateHost.cc
@@ -606,6 +606,9 @@ bool HMIPv6NDStateHost::arhandover(const ipv6_addr& lcoa, IPv6Datagram* dgram)
     }
     typedef cCallbackMessage cbSendMapBUAR;
     cbSendMapBUAR* cb = check_and_cast<cbSendMapBUAR*>(addressCallback(lcoa));
+    //simply called by functions not callback
+    if (cb->arrivalTime() != nd->simTime())
+      return true;
     deleteMe.reset((ipv6_addr*) cb->contextPointer());
   }
   else
@@ -660,10 +663,13 @@ bool HMIPv6NDStateHost::arhandover(const ipv6_addr& lcoa, IPv6Datagram* dgram)
   //and processed it in Superclass::processRtrAd.
   InterfaceEntry *ie = ift->interfaceByPortNo(ifIndex);
 
-  if (rt->odad())
-    assert(ie->ipv6()->addrAssigned(lcoa)||ie->ipv6()->tentativeAddrAssigned(lcoa));
-  else
-    assert(ie->ipv6()->addrAssigned(lcoa));
+  if ((rt->odad() && !(ie->ipv6()->addrAssigned(lcoa)||ie->ipv6()->tentativeAddrAssigned(lcoa))) ||
+      (!rt->odad() && !ie->ipv6()->addrAssigned(lcoa)))
+  {
+      
+      Dout(dc::hmip, rt->nodeName()<<" "<<nd->simTime()<<" arhandover register with MAP "
+	   <<hmipv6cdsMN.currentMap().addr()<<" as lcoa="<<lcoa<<" not bound");
+  }
 
   ipv6_addr oldcoa = hmipv6cdsMN.localCareOfAddr();
   assert(oldcoa != mipv6cdsMN->homeAddr());

Etc/scripts/coredump.rb
@@ -11,9 +11,14 @@ runs = {}
 dumps = []
 exe = File.basename(Dir.pwd)
 corecount = 0
+confs = []
 Dir["core.*"].each{|corefile|
   next if corefile !~ /[.]\d+$/
   dump = `gdb #{exe} -q -x debug.txt #{corefile}|grep ^\#`
+  by = `gdb #{exe} -q -x debug.txt #{corefile}|grep "Core was"`
+#  conf = by.split('`')[1].split(' ')[2]
+#  confs<< conf
+  confs << by
   if dump =~ /\?\?/m
     puts "#{corefile} is stuffed i.e. no bt so deleting"
     File.delete(corefile)
@@ -42,6 +47,9 @@ Dir["core.*"].each{|corefile|
   end
 } #en each core file
 
+File.open("redoConfs", "w"){|f|
+  f.puts confs.join("\n")
+}
 
 puts "There were #{dumps.size} unique dumps out of #{corecount}"
 #puts dumps.to_s

Research/Networks/voip/default.ini
@@ -1,0 +1,233 @@
+#% old-wildcards
+
+[General]
+debug-on-errors = true
+print-undisposed = false
+
+include rng.ini
+
+[OutVectors]
+**.networkInterface.**.enabled = no
+
+
+[Parameters]
+;;test.out default name but if "autocreate" will do netname-#{runno}.out
+**.worldProcessor.datagramTraceFile = ""
+
+;;Default ping6 settings
+**.pingApp.stopTime = 0 ;-> forever
+**.pingApp.startTime = 1
+**.pingApp.interval = 1
+**.pingApp.packetSize = 56
+**.pingApp.hopLimit = 64
+**.pingApp.count = 0 ;-> forever
+**.pingApp.destAddr = ""
+**.pingApp.srcAddr = ""
+**.pingApp.printPing = 1
+;;Default ICMP settings
+**.networkLayer.proc.ICMP.icmpv6Core.icmpRecordRequests = false
+**.networkLayer.proc.ICMP.icmpv6Core.icmpRecordStart = 0
+**.networkLayer.proc.ICMP.icmpv6Core.replyToICMPRequests = true
+**.networkLayer.proc.ICMP.nd.startTime = 0;
+**.networkLayer.proc.ICMP.nd.forwardFromPreviousMap = false;
+
+;;Default MIPv6 settings
+;**.networkLayer.proc.mobility.homeAgent = "0:0:0:0:0:0:0:0"
+**.networkLayer.proc.mobility.homeAgent = ""
+**.networkLayer.proc.mobility.timeInterval = 10
+**.networkLayer.proc.mobility.hopCountThreshold = 1
+
+;;Default UDP apps
+**.numUdpApps = 0
+**.udpAppType = "UDPBasicApp"
+
+;;Default RTP Voip Apps (subset of RTP app)
+;factor used to calculate said value based on sessionBandwidth or actual value
+;when > 1
+;;G.728 values rfc3551
+**.udpApp[*].frameLength = 0.000625
+**.udpApp[*].framesPerPacket = 32
+**.udpApp[*].bitrate = 16000
+**.udpApp[*].lookahead = 0
+**.udpApp[*].stopTime = 0 
+**.udpApp[*].jitterDelay = 0.1
+;;from PacketCable data sheet
+**.udpApp[*].Ie = 7
+**.udpApp[*].Bpl = 17
+
+;;Default RTP app
+**.udpApp[*].destAddrs = ""
+**.udpApp[*].rtcpBandwidth = 0.05 
+**.udpApp[*].startTime = 0 ;Time to start and finish session
+;;these two below r unused in Voip
+**.udpApp[*].packetisationInterval = 0.020
+**.udpApp[*].payloadLength = 40
+**.udpApp[*].port = 5005
+
+;;Default UDP VideoStream client and server setttings
+**.udpApp[*].waitInterval = uniform(.005, .01)
+**.udpApp[*].packetLen = uniform(12000, 15000)
+**.udpApp[*].videoSize = 650e6
+**.udpApp[*].startTime = 0 ; Time to request video stream from server
+**.udpApp[*].localPort = 20403 ; some arbitrary number
+**.udpApp[*].serverAddress = ""
+**.udpApp[*].serverPort = 3088
+
+;;Default TCP apps
+**.numTcpApps = 0
+**.tcpAppType = "TCPSessionApp"
+
+;; Default TCP settings
+**.tcp.sendQueueClass="TCPVirtualDataSendQueue"
+**.tcp.receiveQueueClass="TCPVirtualDataRcvQueue"
+**.tcp.tcpAlgorithmClass="TCPReno"
+**.tcp.recordStats=true
+
+;UDPNode requires IPForward perhaps for IPv4 layer?
+**.IPForward = false
+
+**.max_longitude = 1640
+**.max_latitude = 1640
+**.wlan_speed = 11
+
+;ProcessorManager emulate serial CPU processing
+**.numOfProcessors = 1
+
+;;Processing delays (applies to Router6 only)
+**.ICMP.procDelay = 0 s
+**.fragmentation.procDelay = 0.2 us
+**.multicast.procDelay = 0.5 us
+**.output[*].procDelay = 1 us
+**.send.procDelay = 0 s
+**.tunneling.procDelay = 0 s
+**.localDeliver.procDelay = 0 s
+**.preRouting.procDelay = 0.2 us
+**.forwarding.procDelay = 1 us
+**.inputQueue.procDelay = 0 s
+;;*.router*.icmp.procDelay = 0 s
+;;*.ha*.icmp.procDelay = 0 s
+;;*.ar*.icmp.procDelay = 0 s
+**.networkLayer.proc.forwarding.routingInfoDisplay = false
+
+;;Routing modules
+**.routing6.ra[*].RoutingAlgorithmType="RoutingAlgorithmStatic"
+**.routing6.rp[*].RoutingProtocolType="RoutingProtocolNone"
+**.routing6.RACount = 0
+**.routing6.RPCount = 0
+**.routingTable6.displayIfconfig = false
+**.routingTable6.baseSettings = xmldoc("../../../Etc/empty.xml", "netconf/local[0]")
+
+;;Node parameters
+**.routingTable6.routePackets = false
+**.routingTable6.forwardSitePackets = true
+**.routingTable6.optimisticDAD = false
+**.routingTable6.mobileIPv6Support = false
+**.routingTable6.mobileIPv6Role = "None" ;;(None|HomeAgent|MobileNode) 
+**.routingTable6.routeOptimisation = false
+**.routingTable6.returnRoutability = true
+**.routingTable6.earlyBU = false
+;;**.routingTable6.signalingEnhance = (None|Direct|CellResidency) "None"
+;**.routingTable6.respondBindingRequest (on|off) "off"
+**.routingTable6.eagerHandover = false 
+**.routingTable6.hierarchicalMIPv6Support = false
+**.routingTable6.map = false
+**.routingTable6.mapMNMUSTSetRCoAAsSource = true
+**.routingTable6.edgeHandoverType = ""
+**.routingTable6.ewuOutVectorHODelays = false
+**.routingTable6.mnSendBUAckFlag = false
+**.routingTable6.mipv6BufferPackets = false
+
+
+;Dynamic Topology Models
+**.dtb.topoFilename = "retrieve via XML config unless used separately from IPv6Suite"
+
+;Default AP parameter
+**.chann = intuniform(1, 14)
+**.beaconPeriod = 0.1
+**.authWaitEntryTimeout = 2;
+**.authEntryTimeout = 2;
+**.assEntryTimeout = 120;
+**.consecFailedTransLimit = 3;
+**.ds[*].NWIName="EtherModuleAP"
+**.beginCollectionTime = 0
+**.endCollectionTime = 0
+**.consecFailedTransLimit = 3
+
+;;Default link type
+**.linkLayers[*].NWIName="EtherModule"
+
+;;Default physical layer type
+**.PHYName="PHYSimple"
+
+;;Default mobility handler type
+**.MobilityName="MobilityStatic"
+**.recordMove=false
+
+;;default global movement info
+**.mobilityHandler.moveXmlConfig=xmldoc("../../../Etc/empty.xml", "netconf")
+
+;;Default ../../../Etc/empty.xml config file
+**.IPv6routingFile = xmldoc("../../../Etc/empty.xml")
+
+;;Default wireless parameters. Values come from netconf2.dtd.
+**.networkInterface.ssid = "default"
+**.networkInterface.pathLossExponent = 2.6
+**.networkInterface.pathLossStdDev = 14.1
+**.networkInterface.txPower = 100
+;;Range of 838 meters
+**.networkInterface.thresholdPower = -96
+;;Handover range of 492 meters
+**.networkInterface.hoThresholdPower = -90
+**.networkInterface.probeEnergyTimeout = 0.01
+**.networkInterface.probeResponseTimeout = 0.035
+**.networkInterface.authenticationTimeout = 2000
+**.networkInterface.associationTimeout = 2000
+**.networkInterface.retry = 7
+**.networkInterface.fastActiveScan = false
+**.networkInterface.scanShortCircuit = false
+;;Don't change this to true unless you know what u are doing
+;; and what this means. Ensure proper channel separation 
+;; between adjacent APs otherwise performance suffers a lot
+;; at handover
+**.networkInterface.crossTalk = false
+**.networkInterface.shadowing = false
+**.networkInterface.signalStrengthMaxSample = 1
+;;maximum of 14 only. Ignore MAX_CHANNELS comment in header file
+**.networkInterface.channelsNotToScan = ""
+**.networkInterface.address = ""
+**.networkInterface.dataRate = 11000000 
+**.networkInterface.bandwidthRequirements = 0.5
+**.networkInterface.recordStatisticVector = false
+**.networkInterface.activeScan = true
+**.networkInterface.channelScanTime = 0.2
+**.networkInterface.bufferSize = 100000
+**.networkInterface.linkUpTrigger = false
+;; currently nothing is hooked up to it
+**.networkInterface.linkDownTrigger = false
+**.networkInterface.noAuth = false
+**.networkInterface.registerInterface = true
+**.networkInterface.queueType = "WESingleQueue"
+**.networkInterface.queueSize = -1 ;;infinite
+**.networkInterface.errorRate = 0
+
+;;for WirelessEtherModule a non zero value signifies using it as wireless
+;;propagation delay (unit of ns) otherwise calculated value is used
+;;for PPPv6Module changes the processing delay
+**.networkInterface.procdelay = 0
+
+;Default BR values
+**.recordStats=false
+**.brSrcModel.msgType=0
+**.brSrcModel.destAddr=""
+**.brSrcModel.tStart=0
+**.brSrcModel.bitRate=0
+**.brSrcModel.fragmentLen=0
+
+;Default Dynamic IPv6 CBR Loader
+**.numNodes = 0
+**.rangeMinX = 0
+**.rangeMinY = 0
+**.rangeMaxX = 0
+**.rangeMaxY = 0
+**.srcPrefix = ""
+**.destPrefix= ""

commit 408b4820925a07ce860204c0f71f03cc04f6bbef

fix hmip performance regression introduced by 9206b5ea68e77e5990141a4c2ee7fe222f45ba45 made coredump executable

Etc/scripts/CoreDumpAnalyse.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 File.open("debug.txt", "w"){|f|
   f.puts <<END
 bt

Network/HMIPv6/HMIPv6NDStateHost.cc
@@ -607,7 +607,7 @@ bool HMIPv6NDStateHost::arhandover(const ipv6_addr& lcoa, IPv6Datagram* dgram)
     typedef cCallbackMessage cbSendMapBUAR;
     cbSendMapBUAR* cb = check_and_cast<cbSendMapBUAR*>(addressCallback(lcoa));
     //simply called by functions not callback
-    if (cb->arrivalTime() != nd->simTime())
+    if (cb->arrivalTime() > nd->simTime())
       return true;
     deleteMe.reset((ipv6_addr*) cb->contextPointer());
   }